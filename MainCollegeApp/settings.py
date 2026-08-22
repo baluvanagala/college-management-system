4c5af572adbfee54965284724eebc5378c7901cc
baluvanagala <baluvanagala66@gmail.com>
2026-08-22 17:17:18 +0530

    Update ALLOWED_HOSTS for Render deployment

--- MainCollegeApp/settings.py
+++ MainCollegeApp/settings.py
@@ -28,9 +28,11 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "https://college-management-system-0haw.onrender.com/",
-]
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS",
+    "college-management-system-0haw.onrender.com,localhost,127.0.0.1"
+).split(",")
+
 
 
 
